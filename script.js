2ecd1cd8237ca84c6130df5e9af439668ca026fb
Flavio Moreira Terra <FlavioTerraMoreira@gmail.com>
2021-02-10 19:10:45 -0300

    criado os movimentos e a snake

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d');
 let box = 32;
+let snake = [];
+snake[0] = {x: 8 * box, y: 8 * box};
 
-function criarBG() {
+let direction = 'right';
+
+function createBG() {
     context.fillStyle = 'lightgreen';
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-criarBG();
+function createSnake() {
+
+    for(var cont = 0; cont < snake.length; cont++) {
+        context.fillStyle = 'green';
+        context.fillRect(snake[cont].x, snake[cont].y, box, box);
+    }
+}
+
+function startGame() {
+    createBG();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == 'right') snakeX += box;
+    if(direction == 'left') snakeX -= box;
+    if(direction == 'up') snakeY -= box;
+    if(direction == 'down') snakeY += box;
+
+    snake.pop();
+
+    let newHead = {x: snakeX, y: snakeY};
+
+    snake.unshift(newHead);
+}
+
+let jogo = setInterval(startGame, 100);
+
